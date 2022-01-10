57c39a9e0ed5688082666ba9a58f5abf004457fe
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-10 17:31:10 -0300

    feat: finished style screen

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Style from './style.module.css';
+import Bob from '../../assets/bobo.jpeg';
 
 export default function Card() {
   return (
     <article className={Style.container}>
-      <h1 className={Style.tile}>Crianca cai do avaiao</h1>
-      <p>
-        Rafael Natal is officially OUT of the U.S. Open ... the tennis legend said Tuesday its just
-        too damn unsafe for him to travel to America during the COVID-19 pandemic.
-        The situation is very complicated worldwide, Nadal wrote in a statementThe…
-      </p>
+      <img className={Style.imgTop} src={Bob} alt="logo top car" />
+      <h1 className={Style.title}>Crianca cai do avaiao</h1>
       <time className={Style.date}> 123434 </time>
     </article>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,19 @@ import Style from './style.module.css';
 export default function Home() {
   return (
     <main className={Style.container}>
-      <Card />
+      <h1 className={Style.title}>Seu saite de noticias</h1>
+      <section className={Style.section}>
+        <div className={Style.wrapContent}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
     </main>
   );
 }
